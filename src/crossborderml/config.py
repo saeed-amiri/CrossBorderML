78485efded47fc085f059bb8db8b748a3536d85d
saeed amiri <sdramiri952@gmail.com>
2025-05-27 19:15:45 +0200

    set the paths

---
+++ src/crossborderml/config.py
@@ -0,0 +1,26 @@
+"""Setting the paths and other constant needed in scripts"""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class PathConfig:
+    """Main paths"""
+    project_root: Path = Path(__file__).resolve().parents[2]
+    data_dir: Path = project_root / "data"
+    raw_data_dir: Path = data_dir / "raw"
+    processed_data_dir: Path = data_dir / "processed"
+    sql_dir: Path = project_root / "sql" / "queries"
+    indicator_yaml: Path = \
+        project_root / "src" / "crossborderml" / "conf" / "indicators.yaml"
+    data_readme: Path = data_dir / "README.md"
+
+
+@dataclass(frozen=True)
+class Config:
+    """Binding them together"""
+    paths: PathConfig = PathConfig()
+
+
+CFG = Config()
