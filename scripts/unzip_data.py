8f57d6245f626086221ac987ce0dbfd6b1a84c8f
saeed amiri <sdramiri952@gmail.com>
2025-06-05 15:42:16 +0200

    scripts: update, renaming a module

--- scripts/unzip_data.py
+++ scripts/unzip_data.py
@@ -2,7 +2,7 @@
 Unzip the data in the `raw` directory
 """
 
-from crossborderml.pipeline.ingest import run_unzip
+from crossborderml.pipeline.fetch_extract import run_unzip
 
 
 if __name__ == "__main__":
